43fa8fe739c2bf8ac5ad8dcea60ff157d83709c5
ameesha25 <ameeshabaghel2509@gmail.com>
2025-11-06 15:07:20 +0530

    avatar image and remove interview eith ai

--- frontend/src/components/Cards/ProfileInfoCard.jsx
+++ frontend/src/components/Cards/ProfileInfoCard.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../../context/userContext';
-import AvatarImage from '../../assets/Avatar Copy.png';
+import AvatarImage from '../../assets/avatar.png';
 const ProfileInfoCard = () => {
     const { user, clearUser } = useContext(UserContext);
     const navigate = useNavigate();
